ef58aa86455f845aa6b4884cb1f055c66d4b72f0
Ruthwik Alamuru <ruthwickalamuru@gmail.com>
2024-09-14 18:29:55 +0530

    halfway?

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("okay");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+      var nav = document.getElementById('nav');
+      var yztext = document.getElementById('yztext');
+  
+      if (nav && yztext) {
+        if (window.scrollY > 0) {
+          nav.style.height = '40px';
+          yztext.classList.add('small');
+        } else {
+          nav.style.height = '80px';
+          yztext.classList.remove('small');
+        }
+      }
+    });
+  });
+  
+  
+  
